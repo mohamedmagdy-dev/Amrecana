fb428a74f58e2b7aaaa99baf420559e1aad1d4e3
Mohamed Magdy <mohamedyout51@gmail.com>
2023-04-16 14:00:16 +0200

    End 80% From Home Page

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
-
 let showMenuBtn = document.querySelector("header .container .langs i ");
 let closeMenuBtn = document.querySelector("header .container .links_small i");
 let headerMenu = document.querySelector("header .container .links_small");
 
-let smallLinkAbout = document.querySelector("header .container .links_small ul li.drop_down");
-let smallLinkAboutContent = document.querySelector("header .container .links_small ul li.drop_down ul");
+let smallLinkAbout = document.querySelector(
+  "header .container .links_small ul li.drop_down"
+);
+let smallLinkAboutContent = document.querySelector(
+  "header .container .links_small ul li.drop_down ul"
+);
 
-
+let brandsCards = document.querySelectorAll(
+  ".brands .container .brands_cards .card"
+);
 
 showMenuBtn.onclick = () => {
   headerMenu.classList.add("active");
@@ -16,37 +21,49 @@ closeMenuBtn.onclick = () => {
   headerMenu.classList.remove("active");
 };
 
-
 smallLinkAbout.onclick = () => {
   smallLinkAboutContent.classList.toggle("active");
-}
+};
 
-// slider in home page 
+// slider in home page
 let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex - 1].style.display = "block";
 }
 
-// Set Auto Play Nex
-setInterval(()=>{
-  plusSlides(-1)
-},7000)
+// Set Auto Play Next
+setInterval(() => {
+  plusSlides(-1);
+}, 7000);
+
+
+
+// Brands Card Flip
+Array.from(brandsCards).forEach((el) => {
+  el.onclick = () => {
+    el.classList.toggle("active");
+  };
+});
